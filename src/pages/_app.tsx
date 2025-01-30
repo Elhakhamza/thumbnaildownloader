f5a7266fc0391a133e2eebdc939fab9849027f00
Elhakhamza <elhakhamza91@gmail.com>
2025-01-30 20:06:02 +0000

    Added Google Analytics integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,38 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      window.dataLayer.push(arguments);
+    }
+    gtag("js", new Date());
+    gtag("config", "G-JPRJB65R1Y"); // Your Google Analytics Measurement ID
+  }, []);
+
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-JPRJB65R1Y`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JPRJB65R1Y', { page_path: window.location.pathname });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
