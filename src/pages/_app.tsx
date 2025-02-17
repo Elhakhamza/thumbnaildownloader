277ff445f20512d9b4a69c839400c651fa7ac696
Elhakhamza <86988538+Elhakhamza@users.noreply.github.com>
2025-02-17 20:38:47 +0100

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,10 @@
+// Declare global type for dataLayer
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
@@ -6,19 +13,18 @@ import Script from "next/script";
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      window.dataLayer.push(arguments);
+    function gtag(...args: any[]) {
+      window.dataLayer.push(args);
     }
     gtag("js", new Date());
-    gtag("config", "G-JPRJB65R1Y"); // Your Google Analytics Measurement ID
+    gtag("config", "G-JPRJB65R1Y", { page_path: window.location.pathname });
   }, []);
 
   return (
     <>
-      {/* Google Analytics Script */}
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=G-JPRJB65R1Y`}
+        src="https://www.googletagmanager.com/gtag/js?id=G-JPRJB65R1Y"
       />
       <Script
         id="google-analytics"
