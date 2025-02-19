e8c5487e7bd25569553334b885a5209ae10d680a
Elhakhamza <86988538+Elhakhamza@users.noreply.github.com>
2025-02-19 01:33:13 +0100

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,17 +1,30 @@
-const Footer = () => (
-  <footer className="w-full bg-gray-800 text-white text-center py-4">
-    <p>
-      © {new Date().getFullYear()} Thumbnail-Downloader | Erstellt mit ❤️ von Ihrem{' '}
-      <a
-        href="https://www.thumbnaildownloader.de"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-400 hover:underline"
-      >
-        thumbnaildownloader
-      </a>
-    </p>
-  </footer>
-);
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="w-full bg-gray-800 text-white text-center py-4">
+      <small>
+        © {year} Thumbnail-Downloader |{' '}
+        <a
+          href="https://github.com/Elhakhamza/thumbnaildownloader"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 hover:underline"
+        >
+          Erstellt mit ❤️
+        </a>{' '}
+        von{' '}
+        <a
+          href="https://www.thumbnaildownloader.de"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 hover:underline"
+        >
+          ThumbnailDownloader
+        </a>
+      </small>
+    </footer>
+  );
+};
 
 export default Footer;
